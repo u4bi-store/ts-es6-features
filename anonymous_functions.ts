c3d8967d8de91437cdbd6e96f8fb8f3777569521
u4bi <u4bi@u4bi.com>
2016-12-26 20:11:12 +0000

    es6 anonymous functions

---
+++ anonymous_functions.ts
@@ -0,0 +1,15 @@
+const f = (x, y) => x+y;
+const p = ["u4bi_a", "u4bi_b", "u4bi_c", "u4bi_d"]
+
+const l = p.map(s => s.length)
+  .reduce((a,b) => a+b, 0)
+
+
+send("anonymous function", l);
+
+function send(title, text){
+  var para = document.createElement("p");
+  var str = document.createTextNode("["+title+"] "+text.toString());
+  para.appendChild(str);
+  document.getElementById("node").appendChild(para);
+}
