6c217a9bf3bd7a4d19cabe758f4dc65e95ecbe1a
u4bi <u4bi@u4bi.com>
2016-12-26 18:56:33 +0000

    ts type err

--- greeter.ts
+++ greeter.ts
@@ -1,6 +1,7 @@
-function greeter(person) {
+function greeter(person: string) {
     return "u4bi" + person;
 }
 
 var user = "u4bi";
+var user_a = [0,1,2]; /* 컴파에러 타입이 다름*/
 document.body.innerHTML = greeter(user);
