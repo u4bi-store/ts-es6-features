42a47a592a63c5732a8c27d525580ddf5861884d
u4bi <u4bi@u4bi.com>
2016-12-26 20:23:08 +0000

    es6 default value, variable param, named param

--- anonymous_functions.ts
+++ anonymous_functions.ts
@@ -1,8 +1,9 @@
 const f = (x, y) => x+y;
-const p = ["u4bi_a", "u4bi_b", "u4bi_c", "u4bi_d"]
+const p = ["u4bi_a", "u4bi_b", "u4bi_c", "u4bi_d"];
 
 const l = p.map(s => s.length)
-  .reduce((a,b) => a+b, 0)
+  .reduce((a,b) => a+b, 0);
+/* https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce*/
 
 
 send("anonymous function", l);
