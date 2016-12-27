8373bc5d91198c8456c6fb90262bb7060015b83e
u4bi <u4bi@u4bi.com>
2016-12-27 07:26:34 +0000

    es6 collections es6-map syntax

--- collections/es6-map.ts
+++ collections/es6-map.ts
@@ -7,6 +7,14 @@ send("object style map", m);
 send("es6 map", data);
 send("es6 map copy", m2);
 
+/* object syntax*/
+const name = m.first+" "+m.last;
+send("object style map name",name);
+
+/* es6 map syntax*/
+const name2 = m2.get("first")+" "+m2.get("last");
+send("es6 map name2",name2);
+
 function send(title, text) {
     var para = document.createElement("p");
     var str = document.createTextNode("[" + title + "] " + text.toString());
