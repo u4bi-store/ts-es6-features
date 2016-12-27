af43e4109bb33684a56a76399b95233c8774cc61
u4bi <u4bi@u4bi.com>
2016-12-27 07:40:23 +0000

    es6 collections set render

---
+++ collections/set.ts
@@ -0,0 +1,19 @@
+const ch = {
+  "a": true,
+  "the": true,
+  "an": true,
+  "and": true
+};
+/* [set] sun,is,star,energy,source */
+
+const str = "The Sun is a Star and an Energy Source";
+
+const word = str.split(" ").map(s => s.toLowerCase()).filter(s => !ch[s]);
+send("set", word)
+
+function send(title, text) {
+    var para = document.createElement("p");
+    var str = document.createTextNode("[" + title + "] " + text.toString());
+    para.appendChild(str);
+    document.getElementById("node").appendChild(para);
+}
