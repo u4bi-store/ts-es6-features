6f9ec775801ea32354c99cd11282dcf004baaf5c
u4bi <u4bi@u4bi.com>
2016-12-27 07:21:35 +0000

    es6 collections es6-map render

---
+++ collections/es6-map.ts
@@ -0,0 +1,15 @@
+const m = {first: "james", last: "bond"};
+
+const data = [["first", "james"], ["last", "bond"]];
+const m2 = new Map(data);
+
+send("object style map", m);
+send("es6 map", data);
+send("es6 map copy", m2);
+
+function send(title, text) {
+    var para = document.createElement("p");
+    var str = document.createTextNode("[" + title + "] " + text.toString());
+    para.appendChild(str);
+    document.getElementById("node").appendChild(para);
+}
