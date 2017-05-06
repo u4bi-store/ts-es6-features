566b3277859cac7da9ceaefcf9d61a139748e377
u4bi <u4bi@u4bi.com>
2017-05-06 21:58:07 +0900

    set filtering

--- set.js
+++ set.js
@@ -19,3 +19,13 @@ console.log(setA, setB);
 for(let x of setB) console.log(x);
 // set spread
 console.log(...setB);
+
+let setC = new Set([1,2,3]);
+
+// set filtering
+console.log('setC : ',setC);
+let filterA_Set = new Set([...setC].map(x => 'filter_'+x ));
+console.log('filterA_Set : ', filterA_Set);
+
+let filterB_Set = new Set([...setC].filter(x => (x % 2) == 0));
+console.log('filterB_Set : ', filterB_Set);
