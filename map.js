caf79fbfd8321542155ad67cb373f2c165a7c533
u4bi <u4bi@u4bi.com>
2017-05-06 21:18:22 +0900

    iterables for entry key value

--- map.js
+++ map.js
@@ -55,3 +55,4 @@ console.log(soulMap);
 for(let key of godMap.keys()) console.log(key);       // iterables for key
 for(let value of godMap.values()) console.log(value); // iterables for value
 for(let entry of godMap.entries()) console.log(entry, entry[0], entry[1]); // iterables for entry
+for(let [good_key, good_value] of godMap.entries()) console.log(good_key, good_value); // iterables for entry key value
