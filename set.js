22c46352a9aebdf1137e234b8809c2b025e7cd79
u4bi <u4bi@u4bi.com>
2017-05-06 21:50:55 +0900

    set iterating spread

--- set.js
+++ set.js
@@ -15,3 +15,7 @@ let setA = new Set(['red', 'green', 'blue']);
 let setB = new Set().add('red').add('green').add('blue');
 console.log(setA, setB);
 
+// set iterating
+for(let x of setB) console.log(x);
+// set spread
+console.log(...setB);
